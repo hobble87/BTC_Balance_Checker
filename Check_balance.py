1ed860bed972a9a3fb72d3bb3f8d9bba0616a793
Christopher Hovell <hobble87@hotmail.com>
2022-05-27 20:51:28 +0100

    Add files via upload

---
+++ Check_balance.py
@@ -0,0 +1,30 @@
+import pandas
+import requests
+import json
+import os
+import random
+import threading
+from bit import Key
+from bitcoin import *
+from colorama import *
+
+
+
+link = "https://api-r.bitcoinchain.com/v1/address/"
+
+with open('Wallet.txt') as wallets_file:
+    for line in wallets_file:
+        target = line
+        try:
+            df = pandas.read_json(link + target)
+            amount = df
+            print(amount['address'].values[0], "BTC balance = ", amount['balance'].values[0])
+            #print(amount['balance'].values[0])
+            if (amount['balance'].values[0] > 0.001):
+                f = open("hits.txt", 'a')
+                f.write(line)
+                f.close()
+            else:
+                print("No Balance")
+        except:
+            pass
